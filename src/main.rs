a972f06eb8c9324dfd89c4e20e147ff0958fb9ca
ShyAssassin <49711232+ShyAssassin@users.noreply.github.com>
2023-04-17 18:34:07 +0200

    added more util and verbose logging

--- src/main.rs
+++ src/main.rs
@@ -14,7 +14,7 @@ fn main() {
     let dotfiles_dir = Path::new(&home_dir).join(".dotfiles");
     let current_dir = env::current_dir().unwrap();
     let args = cli::Cli::parse();
-
+    let verbose = args.opts.verbose;
 
     match args.command {
         cli::CliSubcommand::Init { repository, force } => {
@@ -49,6 +49,7 @@ fn main() {
                         println!("Repository already exists, use --force to overwrite!");
                     } else if dotfiles_dir.exists() && force {
                         println!("Overwriting existing repository...");
+                        // remove existing repository
                         fs::remove_dir_all(&dotfiles_dir).unwrap();
                         create_dir_all(&dotfiles_dir).unwrap();
                         Repository::init(&dotfiles_dir).unwrap();
@@ -75,8 +76,7 @@ fn main() {
                 copy(&full_path, dotfiles_dir.join(dest_path)).unwrap();
             } 
             else if full_path.is_dir() {
-                // get file path relative to home dir
-                util::copy_recursively(&full_path,  dotfiles_dir.join(dest_path)).expect("Failed to copy directory");
+                util::copy_recursively(&full_path,  dotfiles_dir.join(dest_path), verbose).expect("Failed to copy directory");
             }
             else {
                 println!("Cannot find file or directory!")
@@ -84,6 +84,10 @@ fn main() {
         }
 
         cli::CliSubcommand::Remove { file } => {
+            // TODO: look for file in dotfiles dir and remove it, currently we check if the relative path exists but not if the file exists in the dotfiles dir
+            // this means that if the file you are trying to remove isnt in the dotfiles dir, it will error it will construct the path to the file in the dotfiles dir and try to remove it
+            // but if the file doesnt exist in the dotfiles dir, it will error
+
             // create full path to file / dir
             let full_path = current_dir.join(&file);
             // get file path relative to home dir
@@ -93,9 +97,13 @@ fn main() {
                 remove_file(dotfiles_dir.join(dest_path)).expect("Failed to remove file");
             } else if full_path.is_dir() {
                 fs::remove_dir_all(dotfiles_dir.join(dest_path)).expect("Failed to remove directory");
-            } else {
+            } else if !full_path.exists() {
                 panic!("Cannot find file or directory!")
             }
+        }
+
+        cli::CliSubcommand::Deploy { dry_run } => {
+            println!("Deploying dotfiles...");
         }
 
         cli::CliSubcommand::Edit { path, editor } => {
@@ -120,6 +128,7 @@ fn main() {
             cli::print_completions(generator, &mut cmd);
         }
         
+        #[allow(unreachable_patterns)]
         _ => {
             println!("Not implemented yet!");
         }
--- src/util.rs
+++ src/util.rs
@@ -1,17 +1,53 @@
 use std::fs;
 use std::io;
-use std::path::Path;
+use std::env;
+use std::path::{Path, PathBuf};
 
-pub fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>) -> io::Result<()> {
+pub fn copy_recursively(source: impl AsRef<Path>, destination: impl AsRef<Path>, verbose: bool ) -> io::Result<()> {
     fs::create_dir_all(&destination)?;
     for entry in fs::read_dir(source)? {
         let entry = entry?;
         let filetype = entry.file_type()?;
         if filetype.is_dir() {
-            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()))?;
+            copy_recursively(entry.path(), destination.as_ref().join(entry.file_name()), verbose)?;
         } else {
+            if verbose {
+                println!("Copying {:?} --> {:?}", entry.path(), destination.as_ref().join(entry.file_name()));
+            }
             fs::copy(entry.path(), destination.as_ref().join(entry.file_name()))?;
         }
     }
     Ok(())
+}
+
+pub fn get_dotfiles_dir() -> PathBuf {
+    let home_dir = env::var("HOME").unwrap();
+    let dotfiles_dir = Path::new(&home_dir).join(".dotfiles");
+    return dotfiles_dir;
+}
+
+// This function is used to convert a relative path to an absolute path
+// Example: .config/nvim/init.vim -> /home/user/.config/nvim/init.vim
+pub fn get_full_path(file: &PathBuf) -> PathBuf {
+    let current_dir = env::current_dir().unwrap();
+    let full_path = current_dir.join(&file);
+    return full_path;
+}
+
+// This function is used to convert an absolute path to a full path relative within the dotfiles directory
+// Example: ~/.config/nvim/init.vim -> ~/.dotfiles/.config/nvim/init.vim
+pub fn get_dest_path(file: &PathBuf) -> PathBuf {
+    let dotfile_dir = get_dotfiles_dir();
+    let full_path = get_full_path(&file);
+    let dest_path = dotfile_dir.join(full_path.strip_prefix(&dotfile_dir).unwrap());
+    return dest_path;
+}
+
+// This function is used to convert an absolute path to a relative path relative to the dotfiles directory
+// Example: ~/.config/nvim/init.vim -> .config/nvim/init.vim
+pub fn get_relative_dest_path(file: &PathBuf) -> PathBuf {
+    let dotfile_dir = get_dotfiles_dir();
+    let full_path = get_full_path(&file);
+    let dest_path = full_path.strip_prefix(&dotfile_dir).unwrap();
+    return dest_path.to_path_buf();
 }
